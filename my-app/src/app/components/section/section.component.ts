b6001f59fb942bb299ee24278f119e776f8e3630
Marina Kotova <m.klementyeva@mail.ru>
2023-06-26 11:47:14 +0200

    feat: add styles

--- my-app/src/app/components/section/section.component.ts
+++ my-app/src/app/components/section/section.component.ts
@@ -13,21 +13,21 @@ export class SectionComponent {
       title: "Name Tag",
       creationDate: "9 Nov. 2018",
       duration: '1h 28min',
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
       id: 'Video Course 2',
       title: "Name Tag",
       creationDate: "9 Nov. 2018",
       duration: '1h 28min',
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
     {
       id: 'Video Course 3',
       title: "Name Tag",
       creationDate: "9 Nov. 2018",
       duration: '1h 28min',
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     },
   ]
 }
